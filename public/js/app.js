7c5e96c992222b3c59b04cdb451ee361e238ecf2
lhuet <laurent.huet@gmail.com>
2013-06-13 12:38:28 +0000

    mise en place socket.io

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,23 @@
 
 var myapp = angular.module('Kata', []);
 
-function sensorController($scope) {
+function sensorController($scope, socketService) {
   $scope.tension = 13;
+  socketService.on('sensor', function (data) {
+    $scope.tension = data;
+  });
 };
+
+myapp.factory('socketService', function ($rootScope) {
+  var socket = io.connect();
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    }
+  };
+});
--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var http = require('http');
+var fs = require('fs');
 
 var app = express();
 var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+var sensorFile="/sys/devices/platform/omap/tsc/ain5";
 
 app.use(express.static(__dirname + '/public'));
 
 server.listen(3333);
 console.log("Serveur node à l'écoute sur le port 3333");
+
+
+
+io.sockets.on('connection', function (socket) {
+  readSensor(socket)
+});
+
+
+function readSensor(socket) {
+  fs.readFile(sensorFile, 'utf8', function (error, data) {
+    var tension = Math.round((parseFloat(data)/4096)*1800*2);
+    socket.emit("sensor", tension);
+    setTimeout(function() {
+      readSensor(socket);
+    }, 100);
+  });
+}
