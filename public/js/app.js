14e4bbaf2d8b358f254fb9635ff1ce6f44139b18
lhuet <laurent.huet@gmail.com>
2013-06-13 12:48:38 +0000

    ajout de la musique

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,14 @@
 
 var myapp = angular.module('Kata', []);
 
-function sensorController($scope, socketService) {
+function sensorController($scope, socketService, synthService) {
   $scope.tension = 13;
+  $scope.note="";
   socketService.on('sensor', function (data) {
     $scope.tension = data;
+  });
+  synthService.on('note', function (data) {
+    $scope.note=data;
   });
 };
 
@@ -22,3 +26,56 @@ myapp.factory('socketService', function ($rootScope) {
     }
   };
 });
+
+myapp.factory('synthService', function ($rootScope) {
+  var socket = io.connect();
+  var note="";
+  var delay = 0;
+  var velocity = 127;
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        if (!(args[0]===note)) {
+          var noteValueOff = notes[note];
+          var noteValueOn = notes[args[0]];
+          MIDI.noteOff(0, noteValueOff, delay + 0.75);
+          MIDI.noteOn(0, noteValueOn, velocity, delay);
+          note = args[0];
+        }
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    }
+  };
+});
+
+
+window.onload = function () {
+  MIDI.loadPlugin({
+    soundfontUrl: "./soundfont/",
+    instrument: "acoustic_grand_piano",
+    callback: function() {
+      console.log("Ready to play !");
+      MIDI.setVolume(0, 127);
+    }
+  });
+};
+
+var notes = {
+  "C4": 60,
+  "C4#": 61,
+  "D4": 62,
+  "D4#": 63,
+  "E4": 64,
+  "F4": 65,
+  "F4#": 66,
+  "G4": 67,
+  "G4#": 68,
+  "A4": 69,
+  "A4#": 70,
+  "B4": 71,
+  "C5": 72
+}
+
--- server.js
+++ server.js
@@ -8,6 +8,22 @@ var io = require('socket.io').listen(server);
 
 var sensorFile="/sys/devices/platform/omap/tsc/ain5";
 
+var notes = [
+  {note: "C4", min: 1800, max: 2900},
+  {note: "C4#", min: 1350, max: 1800},
+  {note: "D4", min: 1050, max: 1350},
+  {note: "D4#", min: 850, max: 1050},
+  {note: "E4", min: 720, max: 850},
+  {note: "F4", min: 620, max: 720},
+  {note: "F4#", min: 570, max: 620},
+  {note: "G4", min: 500, max: 570},
+  {note: "G4#", min: 450, max: 500},
+  {note: "A4", min: 400, max: 450},
+  {note: "A4#", min: 370, max: 400},
+  {note: "B4", min: 320, max: 370},
+  {note: "C5", min: 250, max: 320}
+]
+
 app.use(express.static(__dirname + '/public'));
 
 server.listen(3333);
@@ -24,8 +40,24 @@ function readSensor(socket) {
   fs.readFile(sensorFile, 'utf8', function (error, data) {
     var tension = Math.round((parseFloat(data)/4096)*1800*2);
     socket.emit("sensor", tension);
+
+    note = computeMusic(tension)
+    if (note) {
+      socket.emit("note", note);
+    }
+
     setTimeout(function() {
       readSensor(socket);
     }, 100);
   });
 }
+
+function computeMusic(tension) {
+  for (var i=0; i<notes.length; i++) {
+    if ((tension>notes[i].min)&&(tension<notes[i].max)) {
+      return notes[i].note;
+    }
+  }
+}
+
+
